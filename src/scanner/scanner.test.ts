95de9e9321e03c93c39d089d6fce151da797c216
shortercode <iainshorter@gmail.com>
2020-12-17 22:35:25 +0000

    test: add new scanner tests

--- src/scanner/scanner.test.ts
+++ src/scanner/scanner.test.ts
@@ -1,5 +1,5 @@
 import { create_scanner_context } from './scan_context';
-import { scan_identifier, scan_number, scan_string } from './scanner';
+import { scan_comment, scan_identifier, scan_line_comment, scan_number, scan_string } from './scanner';
 
 import type { TextBuffer } from './text_buffer.type';
 
@@ -118,13 +118,63 @@ describe('scan string', () => {
 		});
 	});
 	it('correctly escapes', () => {
-		const ctx = create_scanner_context('""');
+		const ctx = create_scanner_context(String.raw`"\n"`);
 		const buffer: TextBuffer = { start: 0, length: 0 };
 		expect(scan_string(ctx, buffer)).toEqual({
 			type: 'string',
 			start: { column: 1, row: 1},
-			end: { column: 3, row: 1 },
-			value: ''
+			end: { column: 5, row: 1 },
+			value: String.raw`\n`
+		});
+	});
+});
+
+describe('scan line comment', () => {
+	it('updates index', () => {
+		const ctx = create_scanner_context('// test');
+		scan_line_comment(ctx);
+		expect(ctx).toEqual({
+			source: [...'// test'],
+			index: 7,
+			length: 7,
+			column: 8,
+			row: 1
+		});
+	});
+	it('terminates on newline', () => {
+		const ctx = create_scanner_context('// test\nalpha');
+		scan_line_comment(ctx);
+		expect(ctx).toEqual({
+			source: [...'// test\nalpha'],
+			index: 8,
+			length: 13,
+			column: 1,
+			row: 2
+		});
+	});
+});
+
+describe('scan comment', () => {
+	it('updates index', () => {
+		const ctx = create_scanner_context('/**/');
+		scan_comment(ctx);
+		expect(ctx).toEqual({
+			source: [...'/**/'],
+			index: 4,
+			length: 4,
+			column: 5,
+			row: 1
+		});
+	});
+	it('can consume multiple lines', () => {
+		const ctx = create_scanner_context('/*\n\n\n*/');
+		scan_comment(ctx);
+		expect(ctx).toEqual({
+			source: [...'/*\n\n\n*/'],
+			index: 7,
+			length: 7,
+			column: 3,
+			row: 4
 		});
 	});
 });
