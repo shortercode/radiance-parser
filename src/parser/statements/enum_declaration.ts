5248f8199f0cf8f3db125dfb66a8866ab98c1833
shortercode <iainshorter@gmail.com>
2020-12-24 17:21:37 +0000

    add: stub remaining statement parselets

---
+++ src/parser/statements/enum_declaration.ts
@@ -0,0 +1,6 @@
+import type { ParserContext } from '../parser_context.type';
+import type { EnumDeclaration } from '../statements.type';
+
+export function parse_enum_declaration (ctx: ParserContext): EnumDeclaration {
+	
+}
---
+++ src/parser/statements/struct_declaration.ts
@@ -0,0 +1,6 @@
+import type { ParserContext } from '../parser_context.type';
+import type { StructDeclaration } from '../statements.type';
+
+export function parse_struct_declaration (ctx: ParserContext): StructDeclaration {
+	
+}
